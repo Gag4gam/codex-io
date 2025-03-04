e2e96bdb5eac21a9bbdfefad19ce4417d8bc104a
Gabriel Santos <gabriel__cortes@hotmail.com>
2025-03-04 20:04:19 -0300

    update

---
+++ JavaScript/2/06-Let&Const.js
@@ -0,0 +1,19 @@
+/*const userId = "121113212311";
+let userName = "John Doe";
+console.log("Eu nao acredito que", userId,"é", userName); //Eu nao acredito que 121113212311 é John Doe
+
+userName = "Rosane";
+console.log("Eu nao acredito que", userId,"é", userName); //Eu nao acredito que 121113212311 é Rosane 
+*/
+///exercicio, 4 variasveis para user profile em website
+
+const nome = "John Doe"; //nome do usuario & variavel constante
+const favColor = "blue"; //cor favorita do usuario & variavel constante
+let location = "New York"; //localizacao do usuario & variavel mutavel
+let mood = "happy"; //humor do usuario & variavel mutavel
+
+console.log("Perfil de usuario:",nome, favColor, location, mood); //John Doe blue New York happy
+
+mood = "sad"; //mudando o humor do usuario
+
+console.log("Perfil de usuario:",nome, favColor, location, mood); //John Doe blue New York sad
---
+++ JavaScript/2/07-DataType.js
@@ -0,0 +1,48 @@
+/*//variaveis
+let year = 2023;
+let age = 28;
+let mealCost = 12.99;
+const pi = 3.14;
+const daysOfWeek = 7;
+
+console.log("Year:", year);
+console.log("Age:", age);  
+console.log("Meal Cost:", mealCost);
+console.log("PI:", pi);
+console.log("Days of Week:", daysOfWeek);
+
+//strings
+let message = "good nite";
+let user = '@zuck';
+const company = "Codédex";
+console.log("\nMessage:", message);
+console.log("User:", user);
+console.log("Company:", company);
+
+//booleans
+let hungry = true;
+let lateToMeeting = false;
+const earthIsFlat = false;
+console.log("\nHungry:", hungry);
+console.log("Late to Meeting:", lateToMeeting);
+console.log("Earth is Flat:", earthIsFlat);
+
+//undefined
+let startDate = "\n01/23/2022";
+let endDate; //apeans variaveis tipo let podem ser mudadas
+
+console.log(startDate);  // 01/23/2022
+console.log(endDate);    // undefined
+
+*/
+
+//exercicio
+const companyName = "GabLab";
+const foundingYear = 2003;
+let isActive = true;
+let foundingAmount;
+
+console.log("Company Name:", companyName);
+console.log("Founding Year:", foundingYear);
+console.log("Is Active:", isActive);
+console.log("Founding Amount:", foundingAmount);
